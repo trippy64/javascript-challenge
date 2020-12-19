69b3465b3d15c58b624e948f8976bca5f997fc92
William Taylor <65783939+trippy64@users.noreply.github.com>
2020-12-18 21:47:51 -0500

    adding js progress

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,34 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//console.log(tableData)
+
+
+// Creating variable
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
