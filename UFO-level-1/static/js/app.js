3d27b1924afcb97dd5c46d444d48d2b3d75a1e89
William Taylor <65783939+trippy64@users.noreply.github.com>
2020-12-21 15:24:30 -0500

    pushing option 1 up

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,53 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+//console.log(tableData)
+
+
+// Creating variable
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    
+
+    var inputDate = inputFieldDate.property("value").trim();
+ 
+
+
+var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+
+$tbody.html("");
+
+let response = {
+    filterDate
+}
+
+
+if(response.filterDate.length !== 0) {
+    addData(filterDate);
+}
+
+else {
+    $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+}
+})
